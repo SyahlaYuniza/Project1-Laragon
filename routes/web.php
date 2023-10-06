19583096f1f0f42422a812bac68d7373607ad97c
Syahla Yuniza <yunizasyahla@gmail.com>
2023-10-06 08:32:35 +0800

    Project1 Laragon

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/mahasiswa', function () {
+
+    $array_nama = ["Risa", "Rudi", "Bambang"];
+    return view('universitas.mahasiswa')->with('mahasiswa', $array_nama);
+});
+
+Route::get('/hello', function () {
+    return 'Hello world';
+});
+
+Route::get('/admin', function () {
+    return view ('dashboard');
+});
